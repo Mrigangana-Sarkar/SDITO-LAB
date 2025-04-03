5f0e123b71da4a09ae2857bddfe72fc91cb3ffaf
Mrigangana-Sarkar <mriganganasarkar@gmail.com>
2025-04-03 11:10:45 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Mrigangana Sarkar')
+  res.send('Mrigangana Sarkar ')
 })
 
 app.get('/courses', function (req, res) {
